a21805ffaed6b4730820314d4a0a72aee70e3514
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-12-02 23:07:20 +0200

    minor optimization

--- cinema/views.py
+++ cinema/views.py
@@ -15,12 +15,14 @@ from cinema.serializers import (
 
 class GenreList(APIView):
 
-    def get(self, request) -> Response:
+    @staticmethod
+    def get(request) -> Response:
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request) -> Response:
+    @staticmethod
+    def post(request) -> Response:
         serializer = GenreSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -29,7 +31,8 @@ class GenreList(APIView):
 
 class GenreDetail(APIView):
 
-    def get_object(self, pk):
+    @staticmethod
+    def get_object(pk):
         return get_object_or_404(Genre, pk=pk)
 
     def get(self, request, pk) -> Response:
