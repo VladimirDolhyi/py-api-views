5a636cc6a448888216c3aa318152e60fe0b6e6c0
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-12-02 22:02:41 +0200

    fix Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -31,9 +31,9 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinemahalls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinemahalls/<int:pk>/",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"
     ),
